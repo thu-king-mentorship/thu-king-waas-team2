2ed6b81e217d02293e9d7641cab195c14359df3d
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2025-04-15 09:02:27 +0200

    Implemented lightable objects and fixed wrong layer for outdoor elements

---
+++ Assets/Scripts/Controllers/LightableObjectController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace WAAS.Controllers
+{
+    /// <summary>
+    /// Class <c>LightableObjectController</c> is a script that controls the light of an object.
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class LightableObjectController : MonoBehaviour
+    {
+        /// <value>Property <c>_light</c> represents the Light component attached to the object.</value>
+        [SerializeField]
+        private Light lightSource;
+
+        /// <summary>
+        /// Method <c>Awake</c> is called when the script instance is being loaded.
+        /// </summary>
+        private void Awake()
+        {
+            if (lightSource == null)
+                lightSource = GetComponentInChildren<Light>();
+            lightSource.enabled = false;
+        }
+
+        /// <summary>
+        /// Method <c>OnTriggerEnter</c> is called when the collider other enters the trigger.
+        /// </summary>
+        /// <param name="other">The collider that entered the trigger.</param>
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Lighter"))
+                EnableLight();
+        }
+
+        /// <summary>
+        /// Method <c>EnableLight</c> enables the light of the object.
+        /// </summary>
+        private void EnableLight()
+        {
+            lightSource.enabled = true;
+        }
+    }
+}
